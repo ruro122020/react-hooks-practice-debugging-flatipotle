b08e8bc88dddb6326be1f3d4beac8a1507b33d0c
Ruth <ruro122020@gmail.com>
2023-11-12 12:25:12 -0500

    export SideForm component

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,10 @@
+/*
+Deliverables:
+1. A user should be able to see an order form with options for protein, fillings, toppings, and sides.
+2. A user should be able to select as many proteins, fillings, toppings, and sides as they desire.
+3. A user should be able to submit the form with their selections and see their order(s) listed under "All Orders."
+4. If a user selects any sides, they should be able to click on a button to view their side choices.
+ */
 import React, { useState } from "react";
 import Form from "./Form";
 import Order from "./Order";
